85b49b604ae8df78e41de934f29b5a06dd111cd0
shallsus <shallsuskim@gmail.com>
2025-05-20 16:10:29 +0900

    jinsumerge

--- Assets/02_Scripts/CombatSystem/CombatEvent.cs
+++ Assets/02_Scripts/CombatSystem/CombatEvent.cs
@@ -7,7 +7,7 @@ public class CombatEvent : MonoBehaviour
     public IDamageAble Sender { get; set; }
     public IDamageAble Receiver { get; set; }
     public int Damage { get; set; }
-    public Collider colider { get; set; }
+    public Collider Collider { get; set; }
     public bool UseEffect { get; set; }
     public Vector3 EffectPosition { get; set; }
     public string EffectName { get; set; }
