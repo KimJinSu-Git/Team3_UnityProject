fca8680b40d82f0ca0f24c35884bcaa20ac9c696
jmg0910 <yws33221@gmail.com>
2025-05-22 21:02:08 +0900

    250522_덱<>인벤토리 유아이 연동 성공
    위치변경으로 인한 덱 유아이 연동 성공
    스크립트 최적화 조금 했습니다.
    
    -------
    내일 할것.
    매칭시스템 공동작업
    플레이어 월렛관련
    파이어베이스 연동

---
+++ Assets/02_Scripts/UI/Deck&Collection/SlotUI.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum SlotMode { Deck, Collection }
+
+public class SlotUI : MonoBehaviour
+{
+    [Header("공용")]
+    public Image icon;
+    public TMP_Text nameText, levelText, costText, progressText;
+
+    [Header("버튼")]
+    public Button actionButton;
+    public TMP_Text actionButtonText;
+    public Button infoButton;
+    public GameObject infoPanel;
+    
+    private DeckManager_UI deckManager;
+    private MonsterData_Mainmenu cardData;
+    private int slotIndex;
+    private SlotMode mode;
+    
+    private void Awake()
+    {
+        // 보통 Setup() 또는 Start()에서 초기화
+        infoButton.onClick.AddListener(OnClickSlot);
+        infoPanel.SetActive(false); // 시작 시 꺼두기
+    }
+    
+    public void OnClickSlot()
+    {
+        bool isActive = infoPanel.activeSelf;
+        infoPanel.SetActive(!isActive); // 토글
+    }
+
+    public void Setup(MonsterData_Mainmenu card, int index, DeckManager_UI manager, SlotMode slotMode)
+    {
+        cardData = card;
+        slotIndex = index;
+        deckManager = manager;
+        mode = slotMode;
+
+        icon.sprite = card.icon;
+        nameText.text = card.monsterName;
+        levelText.text = $"Lv.{card.level}";
+        costText.text = card.cost.ToString();
+        progressText.text = "";
+
+        SetupModeUI();
+    }
+
+    private void SetupModeUI()
+    {
+        actionButton.onClick.RemoveAllListeners();
+
+        if (mode == SlotMode.Deck)
+        {
+            actionButtonText.text = "제거";
+            actionButton.onClick.AddListener(() => deckManager.RemoveCard(slotIndex));
+        }
+        else if (mode == SlotMode.Collection)
+        {
+            actionButtonText.text = "사용";
+            actionButton.onClick.AddListener(() => deckManager.TryAddCard(cardData));
+        }
+
+        infoButton.onClick.AddListener(() =>
+            Debug.Log($"[Info] {cardData.monsterName} - {cardData.description}")
+        );
+    }
+}
