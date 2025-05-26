c4e0ebf3d70d9dfa25fb6d19ac4f9b6f1c009a69
shallsus <shallsuskim@gmail.com>
2025-05-26 20:06:37 +0900

    Video 코드 일부 수정

--- Assets/02_Scripts/UI/MainButtonUI.cs
+++ Assets/02_Scripts/UI/MainButtonUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
@@ -119,7 +120,6 @@ public class MainButtonUI : MonoBehaviour
             }
         }
     }
-    
 
     #endregion
     
