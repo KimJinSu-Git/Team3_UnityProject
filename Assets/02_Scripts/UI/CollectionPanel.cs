28d0f5fab408fdc6342d5c3818cb2789d8497401
jmg0910 <yws33221@gmail.com>
2025-05-26 15:38:55 +0900

    5월 26일 UI작업
    컬렉션 스크롤 추가, 애로우레인, 파이어볼 이미지 추가
    교체 작업 추가

--- Assets/02_Scripts/UI/CollectionPanel.cs
+++ Assets/02_Scripts/UI/CollectionPanel.cs
@@ -10,8 +10,8 @@ public class CollectionPanel : MonoBehaviour
     public DeckManager_UI deckManager;
 
     public void Start()
-    {
-      //  Refresh(); // ← 이거 빠지면 아무 슬롯도 생성 안 됨
+    { 
+        Refresh(); // ← 이거 빠지면 아무 슬롯도 생성 안 됨
     }
     
     public void Refresh()
@@ -19,9 +19,16 @@ public class CollectionPanel : MonoBehaviour
         foreach (Transform child in collectionGrid)
             Destroy(child.gameObject);
 
+        Debug.Log($"[CollectionPanel] 카드 개수: {allCards.Count}");
         foreach (var card in allCards)
         {
-            if (deckManager.IsInDeck(card.id)) continue;
+            Debug.Log($"[CollectionPanel] 생성 시도: {card.monsterName}");
+
+            if (deckManager.IsInDeck(card.id))
+            {
+                Debug.Log($"[CollectionPanel] 이미 덱에 포함된 카드: {card.monsterName}");
+                continue;
+            }
 
             var slot = Instantiate(slotPrefab, collectionGrid).GetComponent<SlotUI>();
             slot.Setup(card, -1, deckManager, SlotMode.Collection);
