c136b86e24f03614c0ac89dd30da373125c356ae
KimJinSu-Git <creeper31109@gmail.com>
2025-05-29 20:39:01 +0900

    Team_Project_3.28.5
    왕관 여러개 획득 현상 수정.

--- Assets/02_Scripts/Managers/GameManager.cs
+++ Assets/02_Scripts/Managers/GameManager.cs
@@ -99,7 +99,7 @@ public class GameManager : NetworkBehaviour
         gameStartTime = Time.time;
     }
 
-    [Rpc(sources: RpcSources.All, targets: RpcTargets.All)]
+    [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
     public void RPC_OnPrincessTowerDestroyed(TowerController tower)
     {
         if (currentState != GameState.Playing ) return;
